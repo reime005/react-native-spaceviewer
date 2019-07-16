44d5772586a8962084f2d4e243e4d714566ce45c
Marius Reimer <reime005@gmail.com>
2019-07-16 22:22:29 +0200

    hack to enable bundle in prod

--- scripts/postInstallHacks.sh
+++ scripts/postInstallHacks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-#cp ./scripts/metro/getAssets.js ./node_modules/metro/src/DeltaBundler/Serializers/getAssets.js
+cp ./scripts/metro/getAssets.js ./node_modules/metro/src/DeltaBundler/Serializers/getAssets.js
