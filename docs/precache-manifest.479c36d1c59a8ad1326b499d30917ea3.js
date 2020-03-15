00431cab56932d2ba868f602ca070795c0b574d4
Marius Reimer <reime005@gmail.com>
2020-03-15 22:15:37 +0100

    ghp

---
+++ docs/precache-manifest.479c36d1c59a8ad1326b499d30917ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2e22ae0dd12ae83ef6a5b3efc15f41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "059fa1c4fb01a92ff0c5",
+    "url": "./static/js/2.f3c2b4e5.chunk.js"
+  },
+  {
+    "revision": "9a8157459b1682d2e4932e89a1bb2367",
+    "url": "./static/js/2.f3c2b4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62377766e19695f8af51",
+    "url": "./static/js/main.e5f44a58.chunk.js"
+  },
+  {
+    "revision": "dc0fe772e4a2e95d049b",
+    "url": "./static/js/runtime-main.b977be69.js"
+  },
+  {
+    "revision": "c3bdd73a40d75f1db180d7bb88adfe1c",
+    "url": "./static/media/splash.c3bdd73a.png"
+  }
+]);
